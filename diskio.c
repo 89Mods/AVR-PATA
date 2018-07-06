36211379f0fcb2656dbdbe287d7316bb804b87e2
TheGhastModding <luca.horn@gmx.de>
2018-07-06 14:57:20 +0200

    FatFS integration

---
+++ diskio.c
@@ -0,0 +1,166 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2016        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
+#include "diskio.h"		/* FatFs lower layer API */
+#include "ata.h"
+#include <avr/io.h>
+
+/* You could probably treat each drive partition as a seperate drive here, but I'm only going to use the first here */
+#define DEV_HDD		0
+
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+)
+{
+	DSTATUS stat = 0;
+
+	switch (pdrv) {
+	case DEV_HDD :
+		if((PIND & (1 << PD5)) == 0 || ata_getPartitionSize(0) == 0) stat = STA_NOINIT;
+		return stat;
+	}
+	return STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+	DSTATUS stat = 0;
+
+	switch (pdrv) {
+	case DEV_HDD:
+		//Note: It might be a better Idea to initialize the drive in main to do better error handling
+		uint8_t err = ata_init();
+		if(err != 0) return STA_NOINIT;
+		while((PIND & (1 << PD5)) == 0);
+		return stat;
+	}
+	return STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+	DRESULT res = RES_OK;
+
+	switch (pdrv) {
+	case DEV_HDD:
+		//Check if IORDY pin from HDD is high and partition 0 exists (size != 0)
+		if((PIND & (1 << PD5)) == 0 || ata_getPartitionSize(0) == 0) return RES_NOTRDY;
+		uint8_t t = ata_readByte(REG_STAT_CMD); //Also check RDY in status register
+		if(!((t & 0b10000000) == 0 && (t & 0b01000000) != 0)) return RES_NOTRDY;
+		//Until I add a function to read multiple sectors directly, this'll have to do
+		for(UINT i = 0; i < count; i++){
+			uint32_t actualSec = count - i - 1;
+			uint8_t err = ata_readSector(buff, sector + actualSec + ata_getPartitionLocation(0));
+			if(err != 0) return RES_ERROR;
+			if(count == 1 || actualSec == 0) break;
+			for(uint16_t j = 0; j < 512; j++){
+				buff[actualSec * 512 + j] = buff[j];
+			}
+		}
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{	
+	DRESULT res = RES_OK;
+
+	switch (pdrv) {
+	case DEV_HDD:
+		if((PIND & (1 << PD5)) == 0 || ata_getPartitionSize(0) == 0) return RES_NOTRDY;
+		uint8_t t = ata_readByte(REG_STAT_CMD);
+		if(!((t & 0b10000000) == 0 && (t & 0b01000000) != 0)) return RES_NOTRDY;
+		uint8_t buff2[512];
+		for(uint16_t i = 0; i < count; i++) {
+			for(uint16_t j = 0; j < 512; j++) {
+				buff2[j] = buff[i * 512 + j];
+			}
+			uint8_t err = ata_writeSector(buff2, (uint32_t)sector + ata_getPartitionLocation(0)); //Allways offset address by start of partition!
+			if(err != 0) return RES_ERROR;
+		}
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	DRESULT res = RES_PARERR;
+
+	switch (pdrv) {
+	case DEV_HDD:
+		if((PIND & (1 << PD5)) == 0 || ata_getSectorCount() == 0) return RES_NOTRDY;
+		uint8_t t = ata_readByte(REG_STAT_CMD);
+		if(!((t & 0b10000000) == 0 && (t & 0b01000000) != 0)) return RES_NOTRDY;
+		if(cmd == CTRL_SYNC) return RES_OK;
+		if(cmd == GET_BLOCK_SIZE || cmd == GET_SECTOR_SIZE){
+			 *(DWORD*)buff = 512;
+			 return RES_OK;
+		}
+		if(cmd == GET_SECTOR_COUNT){ //Return size of partition 0
+			*(DWORD*)buff = ata_getPartitionSize(0);
+			return RES_OK;
+		}
+		if(cmd == CTRL_TRIM){
+			return RES_OK;
+		}
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
